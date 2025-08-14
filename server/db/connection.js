5653b512ef3a7ce7f09f5f4210d0c30bde3f28c1
DeveshNegi07 <139962103+DeveshNegi07@users.noreply.github.com>
2025-08-15 01:01:07 +0530

    Update connection.js

--- server/db/connection.js
+++ server/db/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 exports.connect = async () => {
   await mongoose
-    .connect("mongodb://127.0.0.1:27017/WorkoutBuddy")
+    .connect(process.env.MONGO_URI)
     .then(console.log("connection is established"))
     .catch((err) => {
       console.log(`error is :${err}`);
